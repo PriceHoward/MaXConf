a8a491100a2715a9c9c44564ed52f3ecfb9aa4a7
Price Howard <61019159+PriceHoward@users.noreply.github.com>
2025-06-16 20:59:23 -0500

    Update wait-for-sql.sh

--- backend/wait-for-sql.sh
+++ backend/wait-for-sql.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
+
+#This file is used to make sure the SQL database is online before we even try to bring
+#the backend online as it requires the DB to be online first.
 echo "Waiting for MySQL..."
 
 while ! nc -z mysql 3306; do
